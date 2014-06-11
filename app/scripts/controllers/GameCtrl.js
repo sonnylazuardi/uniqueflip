f11c6f8c28dc36ad8bd22a040cc88e1880098453
Sonny Lazuardi <sonnylazuardi@gmail.com>
2014-06-11 09:26:06 +0700

    card

--- app/scripts/controllers/GameCtrl.js
+++ app/scripts/controllers/GameCtrl.js
@@ -2,9 +2,12 @@
 
 var GameCtrl = function($scope) {
   $scope.cards = [0,0,0,0,0,0,0,0,0];
-  $scope.unique = Math.floor((Math.random()*9)+1);
-
+  $scope.unique = Math.floor((Math.random()*9));
   $scope.cards[$scope.unique] = 1;
+  $scope.selects = [];
+  $scope.select = function(id) {
+  	$scope.selects.push(id);
+  }
 };
 
 module.exports = GameCtrl;
