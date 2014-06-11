c22c88469dc05213b75926d6bda856267d74374a
Sonny Lazuardi <sonnylazuardi@gmail.com>
2014-06-11 10:16:25 +0700

    fix bug

--- app/scripts/controllers/GameCtrl.js
+++ app/scripts/controllers/GameCtrl.js
@@ -5,19 +5,21 @@ var GameCtrl = function($scope, Status, $location) {
   $scope.unique = Math.floor((Math.random()*9));
   $scope.cards[$scope.unique] = 1;
   $scope.selects = [];
-  $scope.select = function(id) {
-  	if ($scope.cards[id] == 1) {
-  		Status.setStatus(true);
-  		$location.path('/gameover');
-  	}
 
-  	if ($scope.selects.length < 2) {
-  		if ($scope.selects.indexOf(id) == -1)
+  $scope.select = function(id) {
+  	if ($scope.selects.length < 3) {
+  		if ($scope.selects.indexOf(id) == -1) {
   			$scope.selects.push(id);
-  	} else {
-  		Status.setStatus(false);
-  		$location.path('/gameover');
-  	}
+
+        if ($scope.cards[id] == 1) {
+          Status.setStatus(true);
+          $location.path('/gameover');
+        } else if ($scope.selects.length == 3) {
+          Status.setStatus(false);
+          $location.path('/gameover');
+        }
+      }
+  	} 
   }
 };
 
